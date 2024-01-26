4a183d62a44ca5c313c039849619ffa49acbbbc3
Reza Davari <rezazzr@hotmail.com>
2024-01-26 12:45:53 -0500

    base code added

---
+++ merging_trend_eval.py
@@ -0,0 +1,369 @@
+import argparse
+from itertools import combinations
+
+import torch
+import wandb
+from tqdm.auto import tqdm
+
+from src.eval import eval_single_dataset
+from src.task_vectors import (
+    TaskVector,
+    TaskVectorTopKZero,
+    TaskVectorTopKInit,
+    TaskVectorTopKKeep,
+    TaskVectorMiddleKeep,
+    TaskVectorRandomMask,
+)
+
+zeroshot_acc = {
+    "ViT-B-32": {
+        "MNIST": 48.25,
+        "RESISC45": 60.22,
+        "DTD": 44.41,
+        "GTSRB": 32.56,
+        "SVHN": 31.61,
+        "SUN397": 62.92,
+        "EuroSAT": 45.15,
+        "Cars": 59.64,
+    },
+    "ViT-B-16": {
+        "MNIST": 51.80,
+        "RESISC45": 66.35,
+        "DTD": 44.68,
+        "GTSRB": 43.34,
+        "SVHN": 51.98,
+        "SUN397": 65.22,
+        "EuroSAT": 54.52,
+        "Cars": 64.71,
+    },
+    "ViT-L-14": {
+        "MNIST": 76.36,
+        "RESISC45": 71.33,
+        "DTD": 55.37,
+        "GTSRB": 50.55,
+        "SVHN": 58.45,
+        "SUN397": 67.96,
+        "EuroSAT": 61.63,
+        "Cars": 77.94,
+    },
+}
+finetuned_acc = {
+    "ViT-B-32": {
+        "MNIST": 99.69,
+        "RESISC45": 96.11,
+        "DTD": 79.41,
+        "GTSRB": 98.73,
+        "SVHN": 97.46,
+        "SUN397": 74.98,
+        "EuroSAT": 99.70,
+        "Cars": 77.66,
+    },
+    "ViT-B-16": {
+        "MNIST": 99.76,
+        "RESISC45": 96.89,
+        "DTD": 82.07,
+        "GTSRB": 99.17,
+        "SVHN": 97.86,
+        "SUN397": 78.20,
+        "EuroSAT": 99.70,
+        "Cars": 86.79,
+    },
+    "ViT-L-14": {
+        "MNIST": 99.69,
+        "RESISC45": 97.37,
+        "DTD": 84.15,
+        "GTSRB": 99.24,
+        "SVHN": 98.11,
+        "SUN397": 81.96,
+        "EuroSAT": 99.85,
+        "Cars": 92.39,
+    },
+}
+
+
+def main(args: argparse.Namespace):
+    wandb.init(
+        # Set the project where this run will be logged
+        project="task-vector-addition",
+        # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+        name=f"{args.run_name}_alpha_{args.alpha}_beta_{args.beta}",
+        # Track hyperparameters and run metadata
+        config={
+            "model": args.model,
+            "alpha": args.alpha,
+            "method": args.run_name,
+            "beta": args.beta,
+            "gamma": args.gamma,
+            "eval_type": "partial" if args.eval_on_partial_datasets else "full",
+        },
+    )
+    # build and load all the needed task vectors at once
+    if args.run_name == "paper_implementation":
+        task_vectors_dict = {
+            dataset: TaskVector(
+                args.pretrained_checkpoint, f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt"
+            )
+            for dataset in args.data_sets
+        }
+
+    elif args.run_name == "topk_zero":
+        task_vectors_dict = {
+            dataset: TaskVectorTopKZero(
+                pretrained_checkpoint=args.pretrained_checkpoint,
+                finetuned_checkpoint=f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt",
+                top_k=args.beta,
+            )
+            for dataset in args.data_sets
+        }
+    elif args.run_name == "topk_init":
+        task_vectors_dict = {
+            dataset: TaskVectorTopKInit(
+                pretrained_checkpoint=args.pretrained_checkpoint,
+                finetuned_checkpoint=f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt",
+                top_k=args.beta,
+            )
+            for dataset in args.data_sets
+        }
+    elif args.run_name == "topk_keep":
+        task_vectors_dict = {
+            dataset: TaskVectorTopKKeep(
+                pretrained_checkpoint=args.pretrained_checkpoint,
+                finetuned_checkpoint=f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt",
+                top_k=args.beta,
+            )
+            for dataset in args.data_sets
+        }
+    elif args.run_name == "middle_keep":
+        task_vectors_dict = {
+            dataset: TaskVectorMiddleKeep(
+                pretrained_checkpoint=args.pretrained_checkpoint,
+                finetuned_checkpoint=f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt",
+                top_k_keep=args.beta,
+                top_k_remove=args.gamma,
+            )
+            for dataset in args.data_sets
+        }
+    elif args.run_name == "random":
+        task_vectors_dict = {
+            dataset: TaskVectorRandomMask(
+                pretrained_checkpoint=args.pretrained_checkpoint,
+                finetuned_checkpoint=f"{args.checkpoint_path}/{args.model}/{dataset}/finetuned.pt",
+                keep=args.beta,
+            )
+            for dataset in args.data_sets
+        }
+    else:
+        raise ValueError("Unsupported method of task vectors.")
+
+    base_depth = args.evaluation_depth if args.single_level_eval else 0
+    for nb_datasets in tqdm(range(base_depth, args.evaluation_depth + 1)):
+        global_average_acc = 0.0
+        global_average_normalized_acc = 0.0
+        global_imagenet_acc = 0.0
+        nb_subset = 0
+        for data_subsets in tqdm(combinations(args.data_sets, nb_datasets)):
+            alpha = args.alpha
+            average_acc = 0.0
+            average_normalized_acc = 0.0
+
+            if len(data_subsets) == 0:
+                data_subsets = args.data_sets
+                alpha = 0
+
+            task_vectors = [task_vectors_dict[dataset] for dataset in data_subsets]
+            task_vector_sum = sum(task_vectors)
+            image_encoder = task_vector_sum.apply_to(args.pretrained_checkpoint, scaling_coef=alpha)
+
+            if not args.eval_on_imagenet_only:
+                evaluation_dataset = data_subsets if args.eval_on_partial_datasets else args.data_sets
+                for dataset in evaluation_dataset:
+                    if len(evaluation_dataset) == 0:
+                        try:
+                            results = zeroshot_acc[args.model][dataset]
+                        except KeyError:
+                            results = eval_single_dataset(image_encoder, dataset, args)["top1"] * 100.0
+                    else:
+                        results = eval_single_dataset(image_encoder, dataset, args)["top1"] * 100.0
+
+                    normalized_acc = (results / finetuned_acc[args.model][dataset]) * 100.0
+                    wandb.log(
+                        {
+                            "current_task": dataset,
+                            "individual_acc": results,
+                            "individual_normalized_acc": normalized_acc,
+                            "nb_task_vectors": nb_datasets,
+                            "tasks": " ".join([str(t) for t in data_subsets]),
+                        }
+                    )
+                    average_normalized_acc += normalized_acc
+                    average_acc += results
+
+                average_acc /= len(evaluation_dataset)
+                average_normalized_acc /= len(evaluation_dataset)
+                global_average_acc += average_acc
+                global_average_normalized_acc += average_normalized_acc
+                wandb.log(
+                    {
+                        "average_acc": average_acc,
+                        "average_normalized_acc": average_normalized_acc,
+                        "nb_task_vectors": nb_datasets,
+                        "tasks": " ".join([str(t) for t in data_subsets]),
+                    }
+                )
+                if args.eval_on_imagenet_also:
+                    imagenet_results = eval_single_dataset(image_encoder, "ImageNet", args)["top1"] * 100.0
+                    wandb.log(
+                        {
+                            "imagenet_acc": imagenet_results,
+                            "nb_task_vectors": nb_datasets,
+                            "tasks": " ".join([str(t) for t in data_subsets]),
+                        }
+                    )
+                    global_imagenet_acc += imagenet_results
+            else:
+                # only evaluating on imagenet
+                imagenet_results = eval_single_dataset(image_encoder, "ImageNet", args)["top1"] * 100.0
+                wandb.log(
+                    {
+                        "imagenet_acc": imagenet_results,
+                        "nb_task_vectors": nb_datasets,
+                        "tasks": " ".join([str(t) for t in data_subsets]),
+                    }
+                )
+                global_imagenet_acc += imagenet_results
+
+            nb_subset += 1
+
+        if not args.eval_on_imagenet_only:
+            global_average_acc /= nb_subset
+            global_average_normalized_acc /= nb_subset
+
+            wandb.log(
+                {
+                    "global_average_acc": global_average_acc,
+                    "global_average_normalized_acc": global_average_normalized_acc,
+                    "nb_task_vectors": nb_datasets,
+                }
+            )
+            if args.eval_on_imagenet_also:
+                global_imagenet_acc /= nb_subset
+                wandb.log(
+                    {
+                        "global_imagenet_acc": global_imagenet_acc,
+                        "nb_task_vectors": nb_datasets,
+                    }
+                )
+        else:
+            global_imagenet_acc /= nb_subset
+            wandb.log(
+                {
+                    "global_imagenet_acc": global_imagenet_acc,
+                    "nb_task_vectors": nb_datasets,
+                }
+            )
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_location",
+        help="Path to the root data.",
+        default="~/data",
+        type=str,
+    )
+    parser.add_argument(
+        "--run_name",
+        help="Optional name for the run.",
+        type=str,
+        default="paper_implementation",
+        choices=["paper_implementation", "topk_zero", "topk_init", "topk_keep", "middle_keep", "random"],
+    )
+    parser.add_argument(
+        "--checkpoint_path",
+        help="Path to the directory that holds all the checkpoints.",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--model",
+        help="The type of model.",
+        choices=["ViT-B-32", "ViT-B-16", "ViT-L-14"],
+        default="ViT-B-32",
+        type=str,
+    )
+    parser.add_argument(
+        "--data_sets",
+        help="The name of the datasets used for evaluation",
+        choices=["MNIST", "Cars", "RESISC45", "DTD", "GTSRB", "SVHN", "EuroSAT", "SUN397"],
+        default="MNIST",
+        nargs="*",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--evaluation_depth",
+        help="The depth refers to how many task vectors should be added for evaluation.",
+        default=0,
+        type=int,
+    )
+
+    parser.add_argument(
+        "--alpha",
+        help="The value of alpha indicates the task vector multipliers.",
+        default=0.4,
+        type=float,
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=1024,
+    )
+
+    parser.add_argument(
+        "--beta",
+        help="The removal value.",
+        default=0.0,
+        type=float,
+    )
+    parser.add_argument(
+        "--gamma",
+        help="The removal value needed for the middle keep method.",
+        default=0.0,
+        type=float,
+    )
+
+    parser.add_argument(
+        "--single_level_eval",
+        help="Indicates whether we want to evaluate on a single level basis or up to a level.",
+        default=False,
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--eval_on_imagenet_only",
+        help="Run all evaluations on imagenet data only.",
+        default=False,
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--eval_on_imagenet_also",
+        help="Run imagenet evaluation in addition to all other evaluations.",
+        default=False,
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "--eval_on_partial_datasets",
+        help="If used, we evaluate only on the datasets relevant to the task vectors as opposed to all the datasets.",
+        default=False,
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    args.save = f"{args.checkpoint_path}/{args.model}"
+    args.pretrained_checkpoint = f"{args.checkpoint_path}/{args.model}/zeroshot.pt"
+    main(args=args)
---
+++ src/task_vectors.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+from typing import Optional, Dict
+
+import torch
+from torch import Tensor
+from abc import ABC
+
+from src.utils import safe_load_state_dict
+
+
+class TaskVectorABC(ABC):
+    def __init__(
+        self, pretrained_checkpoint=Optional[str], finetuned_checkpoint=Optional[str], vector=Dict[str, Tensor]
+    ):
+        """Initializes the task vector from a pretrained and a finetuned checkpoints.
+
+        This can either be done by passing two state dicts (one corresponding to the
+        pretrained model, and another to the finetuned model), or by directly passying in
+        the task vector state dict.
+        """
+        if vector is not None:
+            self.vector = vector
+        else:
+            assert pretrained_checkpoint is not None and finetuned_checkpoint is not None
+            with torch.no_grad():
+                pretrained_state_dict = safe_load_state_dict(pretrained_checkpoint)
+                finetuned_state_dict = safe_load_state_dict(finetuned_checkpoint)
+                self.vector = {}
+                for key in pretrained_state_dict:
+                    if pretrained_state_dict[key].dtype in [torch.int64, torch.uint8]:
+                        continue
+                    self.vector[key] = finetuned_state_dict[key] - pretrained_state_dict[key]
+
+    def __add__(self, other: TaskVectorABC):
+        """Add two task vectors together."""
+        with torch.no_grad():
+            new_vector = {}
+            for key in self.vector:
+                if key not in other.vector:
+                    print(f"Warning, key {key} is not present in both task vectors.")
+                    continue
+                new_vector[key] = self.vector[key] + other.vector[key]
+        return TaskVectorABC(vector=new_vector)
+
+    def __radd__(self, other: TaskVectorABC):
+        if other is None or isinstance(other, int):
+            return self
+        return self.__add__(other)
+
+    def __neg__(self):
+        """Negate a task vector."""
+        with torch.no_grad():
+            new_vector = {}
+            for key in self.vector:
+                new_vector[key] = -self.vector[key]
+        return TaskVectorABC(vector=new_vector)
+
+    def apply_to(self, pretrained_checkpoint, scaling_coef=1.0):
+        """Apply a task vector to a pretrained model."""
+        with torch.no_grad():
+            pretrained_model = torch.load(pretrained_checkpoint)
+            new_state_dict = {}
+            pretrained_state_dict = pretrained_model.state_dict()
+            for key in pretrained_state_dict:
+                if key not in self.vector:
+                    print(f"Warning: key {key} is present in the pretrained state dict but not in the task vector")
+                    continue
+                new_state_dict[key] = pretrained_state_dict[key] + scaling_coef * self.vector[key]
+        pretrained_model.load_state_dict(new_state_dict, strict=False)
+        return pretrained_model
+
+
+class TaskVector(TaskVectorABC):
+    def __init__(
+        self, pretrained_checkpoint=Optional[str], finetuned_checkpoint=Optional[str], vector=Dict[str, Tensor]
+    ):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+
+
+class TaskVectorTopKZero(TaskVectorABC):
+    def __init__(self, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None, top_k: float = 0):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+        self.top_k = top_k
+        with torch.no_grad():
+            for key, value in self.vector.items():
+                self.vector[key] = self.mask(value)
+
+    def mask(self, tensor: Tensor) -> Tensor:
+        if len(tensor.shape) == 0:
+            return tensor
+        else:
+            top_k_int = int(tensor.shape[-1] * self.top_k)
+            _, masked_indices = torch.topk(torch.abs(tensor), top_k_int)
+            mask = torch.ones(tensor.shape)
+            mask.scatter_(len(tensor.shape) - 1, masked_indices, 0.0)
+
+            return mask * tensor
+
+
+class TaskVectorTopKInit(TaskVectorABC):
+    def __init__(self, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None, top_k: float = 0):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+        self.top_k = top_k
+        pretrained_state_dict = torch.load(pretrained_checkpoint).state_dict()
+        with torch.no_grad():
+            for key, value in self.vector.items():
+                self.vector[key] = self.mask_and_init(value, pretrained_state_dict[key])
+
+    def mask_and_init(self, tensor_a: Tensor, tensor_b: Tensor) -> Tensor:
+        if len(tensor_a.shape) == 0:
+            return tensor_a
+        else:
+            top_k_int = int(tensor_a.shape[-1] * self.top_k)
+            _, masked_indices = torch.topk(torch.abs(tensor_a), top_k_int)
+            mask = torch.ones(tensor_a.shape)
+            mask.scatter_(len(tensor_a.shape) - 1, masked_indices, 0.0)
+
+            return mask * tensor_a + (~mask.bool()).int() * tensor_b
+
+
+class TaskVectorTopKKeep(TaskVectorABC):
+    def __init__(self, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None, top_k: float = 0):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+        self.top_k = top_k
+        with torch.no_grad():
+            for key, value in self.vector.items():
+                self.vector[key] = self.mask(value)
+
+    def mask(self, tensor: Tensor) -> Tensor:
+        if len(tensor.shape) == 0:
+            return tensor
+        else:
+            top_k_int = int(tensor.shape[-1] * self.top_k)
+            _, masked_indices = torch.topk(torch.abs(tensor), top_k_int)
+            mask = torch.zeros(tensor.shape)
+            mask.scatter_(len(tensor.shape) - 1, masked_indices, 1)
+
+            return mask * tensor
+
+
+class TaskVectorMiddleKeep(TaskVectorABC):
+    def __init__(
+        self,
+        pretrained_checkpoint=None,
+        finetuned_checkpoint=None,
+        vector=None,
+        top_k_keep: float = 0,
+        top_k_remove: float = 0,
+        remove_first: bool = True,
+    ):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+        self.top_k_keep = top_k_keep
+        self.top_k_remove = top_k_remove
+        with torch.no_grad():
+            for key, value in self.vector.items():
+                if remove_first:
+                    self.vector[key] = self.mask_keep_top(self.mask_remove_top(value))
+                else:
+                    self.vector[key] = self.mask_remove_top(self.mask_keep_top(value))
+
+    def mask_keep_top(self, tensor: Tensor) -> Tensor:
+        if len(tensor.shape) == 0:
+            return tensor
+        else:
+            top_k_int = int(tensor.shape[-1] * self.top_k_keep)
+            _, masked_indices = torch.topk(torch.abs(tensor), top_k_int)
+            mask = torch.zeros(tensor.shape)
+            mask.scatter_(len(tensor.shape) - 1, masked_indices, 1)
+
+            return mask * tensor
+
+    def mask_remove_top(self, tensor: Tensor) -> Tensor:
+        if len(tensor.shape) == 0:
+            return tensor
+        else:
+            top_k_int = int(tensor.shape[-1] * self.top_k_remove)
+            _, masked_indices = torch.topk(torch.abs(tensor), top_k_int)
+            mask = torch.ones(tensor.shape)
+            mask.scatter_(len(tensor.shape) - 1, masked_indices, 0.0)
+
+            return mask * tensor
+
+
+class TaskVectorRandomMask(TaskVectorABC):
+    def __init__(self, pretrained_checkpoint=None, finetuned_checkpoint=None, vector=None, keep: float = 0):
+        super().__init__(pretrained_checkpoint, finetuned_checkpoint, vector)
+        self.keep = keep
+        with torch.no_grad():
+            for key, value in self.vector.items():
+                self.vector[key] = self.mask_keep_random(value)
+
+    def mask_keep_random(self, tensor: Tensor) -> Tensor:
+        if len(tensor.shape) == 0:
+            return tensor
+        else:
+            mask = torch.rand(tensor.shape) < self.keep
+            return mask * tensor
